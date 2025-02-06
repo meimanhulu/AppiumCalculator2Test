e9922fb7e99878f16d4f5ce36b8437acdeb7a56c
meimanhulu <94421861+meimanhulu@users.noreply.github.com>
2025-02-07 00:34:28 +0700

    push

---
+++ src/main/java/com/calculator2/appium/App.java
@@ -0,0 +1,13 @@
+package com.calculator2.appium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
